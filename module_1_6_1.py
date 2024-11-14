cebaecfa9173dd048d1a4d5c406d04211ab50b54
Konopach <freakazoid361@gmail.com>
2024-11-14 18:11:14 +0300

    /

--- module_1_6_1.py
+++ module_1_6_1.py
@@ -1,3 +1,14 @@
+# grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#
+# sorted_students = sorted(list(students))
+#
+# average_grade = [(sum(grades[0]) / len(grades[0])),(sum(grades[1]) / len(grades[1])),(sum(grades[2]) / len(
+# grades[2])),(sum(grades[3]) / len(grades[3])),(sum(grades[4]) / len(grades[4]))]
+#
+# average_grades = dict(zip(sorted_students, average_grade))
+# print(average_grades)
+
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
