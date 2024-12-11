e5a5ec91b1bbc270474b034df90410cd0a27d58c
Konopach <freakazoid361@gmail.com>
2024-12-11 17:41:30 +0300

    /

--- module_3_1.py
+++ module_3_1.py
@@ -1,22 +1,24 @@
 calls = 0
 
+
 def count_calls():
     global calls
     calls += 1
 
-def string_info (string):
+
+def string_info(string):
     count_calls()
     return (len(string), string.upper(), string.lower())
+
 
 def is_contains(string, list_to_search):
     count_calls()
     string_lower = string.lower()
     return string_lower in (item.lower() for item in list_to_search)
 
+
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic']))  # No matches
 print(calls)
-
-
