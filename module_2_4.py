34412970eb7a759228d330c5aaa53a9b33b15c7e
Konopach <freakazoid361@gmail.com>
2024-12-11 17:20:29 +0300

    /

--- module_2_4.py
+++ module_2_4.py
@@ -17,6 +17,22 @@ for num in numbers:
 print('Primes:', primes)
 print('Not primes:', not_primes)
 
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# primes = []
+# not_primes = []
+# for elem in numbers:
+#     Flag = True
+#     for divizor in range(2, elem):
+#         if elem % divizor == 0:
+#             Flag = False
+#             not_primes.append(elem)
+#             break
+#     if Flag == True and elem != 1:
+#         primes.append(elem)
+#
+# print('Primes:', primes)
+# print('Not primes:', not_primes)
+
 
 
 
